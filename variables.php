144c1619195ebafde1116ed2a5905d419b1151be
Mohammad Sadegh <77042998+mohammadshg84@users.noreply.github.com>
2021-07-06 17:40:41 +0430

    Add files via upload

---
+++ variables.php
@@ -0,0 +1,10 @@
+<?php
+
+define('URL_CALLBACK', 'http://YOUR-DOMAIN/callback.php');
+
+define('URL_PAYMENT', 'https://api.idpay.ir/v1.1/payment');
+define('URL_INQUIRY', 'https://api.idpay.ir/v1.1/payment/inquiry');
+define('URL_VERIFY', 'https://api.idpay.ir/v1.1/payment/verify');
+
+define('APIKEY', 'YOUR-API-KEY');
+define('SANDBOX', FALSE);
