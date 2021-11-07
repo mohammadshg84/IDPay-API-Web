885ec0d1c184842a3c7493179353c2c646d10806
Mohammad Sadegh <77042998+mohammadshg84@users.noreply.github.com>
2021-11-07 08:52:41 +0330

    Update variables.php

--- variables.php
+++ variables.php
@@ -2,7 +2,7 @@
 
 $domain = "YOUR-DOMAIN";
 
-define('URL_CALLBACK', 'http://$domain/callback.php');
+define('URL_CALLBACK', "http://$domain/callback.php");
 
 define('URL_PAYMENT', 'https://api.idpay.ir/v1.1/payment');
 define('URL_INQUIRY', 'https://api.idpay.ir/v1.1/payment/inquiry');
