5b9a3589ab19332d34fa2ad7be22a86a0e1e9b74
Mohammad Sadegh <77042998+mohammadshg84@users.noreply.github.com>
2021-11-07 08:50:53 +0330

    Update index.php

--- index.php
+++ index.php
@@ -8,44 +8,43 @@ $desc = "DESCRIPTION";
 require_once('variables.php');
 
 $params = array(
-  'order_id' => '101',
-  'amount' => $amount,
-  'phone' => '',
-  'name' => $name,
-  'desc' => $desc,
-  'callback' => URL_CALLBACK,
+    'order_id' => '101',
+    'amount' => $amount,
+    'phone' => '',
+    'name' => $name,
+    'desc' => $desc,
+    'callback' => URL_CALLBACK,
 );
 
 idpay_payment_create($params);
 
 function idpay_payment_create($params) {
-    $header = array(
-    'Content-Type: application/json',
-    'X-API-KEY:' . APIKEY,
-    'X-SANDBOX:' . SANDBOX,
-  );
+    $header = array (
+        'Content-Type: application/json',
+        'X-API-KEY:' . APIKEY,
+        'X-SANDBOX:' . SANDBOX,
+    );
 
-  $ch = curl_init();
-  curl_setopt($ch, CURLOPT_URL, URL_PAYMENT);
-  curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
-  curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($params));
-  curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, URL_PAYMENT);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($params));
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
 
-  $result = curl_exec($ch);
-  curl_close($ch);
+    $result = curl_exec($ch);
+    curl_close($ch);
 
-  $result = json_decode($result);
+    $result = json_decode($result);
 
-  if (empty($result) || empty($result->link)) {
-
-    print 'Exception message:';
-    print '<pre>';
-    print_r($result);
-    print '</pre>';
-
-    return FALSE;
-}
-
-  //Redirect to payment form
-  header('Location:' . $result->link);
+    if (empty($result) || empty($result->link))
+    {
+        print 'Exception message:';
+        print '<pre>';
+        print_r($result);
+        print '</pre>';
+        return FALSE;
+    }
+  
+    //Redirect to payment form
+    header('Location:' . $result->link);
 }
